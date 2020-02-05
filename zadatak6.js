e426a36f520283615b3f48c411358cfb7dc01d60
Bojana Tesic <tesic.bojana@yahoo.com>
2020-02-05 22:21:47 +0100

    izmena zadatak6.js

--- zadatak6.js
+++ zadatak6.js
@@ -1,48 +1,29 @@
 let i=0;
 let brojac =0
-for (i>0; brojac<=100; i++){
+for (i=0; brojac<100; i++){
 switch(i){
     case 1:
         if(i%3==0 && i%5==0){
         console.log ('FizzBuzz');}
-        brojac++;
+        // brojac++;
+        break;
 
-        // else {
-        //     console.log(i);
-        // }
-            break;
     case 2:
         if (i%3==0){
         console.log ('Fizz');}
-        brojac++;
-
-        // else {
-        //     console.log (i)
-        // }
+        // brojac++;
         break;
     case 3:
         if (i%5==0){
         console.log ('Buzz');}
-        brojac++;
-
-        // else{
-        //     console.log(i);
-        // }
+        // brojac++;
         break;
-    case 4:
+    default:
         if(i%3!=0 && i%5!=0){
-        console.log ('Fizz');}
-        brojac++;
-
-        // else{
-        //     console.log(i);
-        // }
+        console.log (i);}
+        // brojac++;
         break;
-        
-    default: console.log(i);
-    brojac++;
-    break;
-// }
 }
+brojac++;
 
 }
